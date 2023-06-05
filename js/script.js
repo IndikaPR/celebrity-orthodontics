0a7bfde20ab92b36c4eb5092b55ca390063fda61
IndikaPR <126867054+IndikaPR@users.noreply.github.com>
2023-06-05 07:43:50 +0530

    making celebrity orthodontics

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const swiperTestimonial = new Swiper(".section-testimonial .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperCosmetics = new Swiper(".section-cosmetics .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
